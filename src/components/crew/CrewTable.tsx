d15f9cb4ba83bf5e52a3bd97feae6542caf7aef3
Adm1RalDen <games.zadrotina@gmail.com>
2019-11-05 14:37:26 +0200

    add sort members by the last name

--- src/components/crew/CrewTable.tsx
+++ src/components/crew/CrewTable.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Crew } from '../../emulator/types'
 
-export default function CrewTable (props: { crew: Crew }) {
+export default function CrewTable(props: { crew: Crew }) {
   const { crew } = props
   return <table>
     <tbody>
-      {crew.map(member => <tr key={member.id}>
+      {crew.sort((member1, member2) => member1.lastName > member2.lastName ? 1 : -1).map(member => <tr key={member.id}>
         <td>{member.id}</td>
         <td>{member.lastName}, {member.firstName}</td>
         <td>
